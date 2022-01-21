f950a4e78b9b250806e7d80a73cedd73f7557f47
SawtaAkhade <sawta.akhade@beautosys.com>
2022-01-21 10:03:51 +0530

    add-student-fixes

--- src/app/modules/_examPortal/components/collage/add-update-college/add-update-college.component.ts
+++ src/app/modules/_examPortal/components/collage/add-update-college/add-update-college.component.ts
@@ -1,7 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { CollageService } from '../_services/collage.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-update-college',
@@ -10,100 +7,10 @@ import { CollageService } from '../_services/collage.service';
 })
 export class AddUpdateCollegeComponent implements OnInit {
 
-  submitted: boolean = false;
-  basicInfoForm!: FormGroup;
+  constructor(){}
 
-  isEditable = false;
- 
-  currentDate = new Date();
-
-  constructor(
-    public dialogRef: MatDialogRef<AddUpdateCollegeComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any, private collageservices:CollageService) { }
-
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
-
- 
   ngOnInit() {
-    console.log(this.data);
-      this.basicInfoForm = new FormGroup({
-        collegeName: new FormControl('',[Validators.required]),
-        location: new FormControl('',[Validators.required]),
-        country: new FormControl('',[Validators.required]),
-        state: new FormControl('',[Validators.required]),
-        contactPerName1: new FormControl('',[Validators.required, Validators.pattern('^[a-zA-Z ]*$')]),
-        contactPerEmail1: new FormControl('',[Validators.required, Validators.email]),
-        contactPerName2: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z ]*$')]),
-        contactPerEmail2: new FormControl('',[Validators.required, Validators.email]),
-        // contactPerName3: new FormControl(''),
-        // contactPerName4: new FormControl(''),
-        // contactPerName5: new FormControl(''),
-        // contactPerEmail3: new FormControl(''),
-        // contactPerEmail4: new FormControl(''),
-        // contactPerEmail5: new FormControl(''),
-        contactPer1ContactNo: new FormControl('',[Validators.required]),
-        contactPer2ContactNo: new FormControl('',[Validators.required]),
-        // contactPer3ContactNo: new FormControl(''),
-        // contactPer4ContactNo: new FormControl(''),
-        // contactPer5ContactNo: new FormControl(''),
-        grade: new FormControl(''),
-        yearOfEsta: new FormControl(''),
-        university: new FormControl(''),
-        accredation: new FormControl(''),
-        CollegeUniAffilation: new FormControl('',[Validators.required]),
-        collgeBio: new FormControl(''),
- 
-
-    })
 
   }
 
-  get basicInfoFormControls() {
-    return this.basicInfoForm.controls;
-  }
-
-
-  onSubmit(){
-    this.submitted=true;
-    console.table(this.basicInfoForm.value)
-    if(this.basicInfoForm.valid){
-      var data = {
-        "collegeName":this.basicInfoForm.value.collegeName,
-        "location":this.basicInfoForm.value.location,
-        "contactPerName1":this.basicInfoForm.value.contactPerName1,
-        "contactPerName2":this.basicInfoForm.value.contactPerName2,
-        "contactPerName3":"",
-        "contactPerName4":"",
-        "contactPerName5":"",
-        "contactPerEmail1":this.basicInfoForm.value.contactPerEmail1,
-        "contactPerEmail2":this.basicInfoForm.value.contactPerEmail2,
-        "contactPerEmail3":"",
-        "contactPerEmail4":"",
-        "contactPerEmail5":"",
-        "contactPer1ContactNo":this.basicInfoForm.value.contactPer1ContactNo,
-        "contactPer2ContactNo":this.basicInfoForm.value.contactPer2ContactNo,
-        "contactPer3ContactNo":"",
-        "contactPer4ContactNo":"",
-        "contactPer5ContactNo":"",
-        "grade":this.basicInfoForm.value.grade,
-        "yearOfEsta":this.basicInfoForm.value.yearOfEsta,
-        "university":this.basicInfoForm.value.university,
-        "accredation":this.basicInfoForm.value.accredation,
-        "country":this.basicInfoForm.value.country,
-        "state":this.basicInfoForm.value.state,
-        "CollegeUniAffilation":this.basicInfoForm.value.CollegeUniAffilation,
-        "collgeBio":this.basicInfoForm.value.collgeBio,
-   }
-   this.collageservices.addCollageDetails(data).subscribe(
-     response=>{
-console.log(response);
-     }
-   );
-    }
-    
-
-
-  }
 }
--- src/app/modules/_examPortal/components/student/add-student/add-student.component.ts
+++ src/app/modules/_examPortal/components/student/add-student/add-student.component.ts
@@ -1,25 +1,78 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { HeaderTitleService } from 'src/app/theme/header/header-title.service';
+import { CollageService } from '../../collage/_services/collage.service';
 
 @Component({
   selector: 'app-add-student',
   templateUrl: './add-student.component.html',
-  styleUrls: ['./add-student.component.scss']
+  styleUrls: ['./add-student.component.scss'],
+  providers: [DatePipe]
 })
 export class AddStudentComponent implements OnInit {
   studentForm:FormGroup;
   submitted:boolean=false;
-  constructor(private activatedRoute:ActivatedRoute, private headerTitleService:HeaderTitleService) {
+  communicationSkill:string[]=[
+    'Written Communication',
+    'Oral Communication',
+    'Presentation',
+    'Active Listening',
+    'Nonverbal Communication',
+    'Feedback',
+    'Respect  ',
+    'Confidence',
+    'Clarity ',
+    'Honesty ',
+    'Friendliness '
+  ];
+  techSkill:string[]=[
+    'Data Management',
+    'Business Analysis',
+    'Accounting',
+    'Project Management',
+    'Engineering',
+    'Marketing',
+    'Medicine',
+    'Coding or Programming',
+    'Graphic Design',
+    'Productivity Software'
+  ];
+  languageSkill:string[]=[
+    'English',
+    'Hindi',
+    'Marathi',
+    'Tamil',
+    'Kannada',
+    'Telugu',
+    'Malayalam',
+    'Odia',
+    'Kashmiri',
+    'Manipuri',
+    'Punjabi',
+    'Urdu'
+  ];
+  countryList:string[]=[];
+  countryOptions: Observable<string[]> | undefined;
+  csOptions: Observable<string[]> | undefined;
+  tsOptions: Observable<string[]> | undefined;
+  languageOptions: Observable<string[]> | undefined;
+
+  todayDate;
+  currentDate = new Date();
+  constructor(private datePipe: DatePipe,private activatedRoute:ActivatedRoute, private headerTitleService:HeaderTitleService, private collageservices:CollageService) {
+    this.todayDate = this.datePipe.transform(this.currentDate, 'yyyy-MM-dd');
     this.studentForm=new FormGroup({
-      firstName:new FormControl(''),
-      lastName:new FormControl(''),
-      fatherName:new FormControl(''),
-      motherName:new FormControl(''),
+      firstName:new FormControl('',[Validators.required, Validators.pattern('^[a-zA-Z ]*$')] ),
+      lastName:new FormControl('',[Validators.required, Validators.pattern('^[a-zA-Z ]*$')]),
+      fatherName:new FormControl('',[Validators.required, Validators.pattern('^[a-zA-Z ]*$')]),
+      motherName:new FormControl('',[Validators.required, Validators.pattern('^[a-zA-Z ]*$')]),
       dateOfBirth:new FormControl(''),
-      phoneNumber:new FormControl(''),
-      alternatePhoneNumber:new FormControl(''),
+      phoneNumber:new FormControl('',[Validators.required, Validators.maxLength(20),Validators.pattern('^[0-9]*$')]),
+      alternatePhoneNumber:new FormControl('',[Validators.required,Validators.maxLength(20), Validators.pattern('^[0-9]*$')]),
       address:new FormControl(''),
       briefDescriptionAboutStudent:new FormControl(''),
       
@@ -28,7 +81,7 @@ export class AddStudentComponent implements OnInit {
       studentCommunicationSkills:new FormControl(''),
       studentTechnicalSkills:new FormControl(''),
       studentLanguageKnown:new FormControl(''),
-      county:new FormControl(''),
+      country:new FormControl(''),
       state:new FormControl(''),
       studentDepartment:new FormControl(''),
       studentSemester:new FormControl(''),
@@ -38,17 +91,69 @@ export class AddStudentComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    this.getCountryList();
     this.activatedRoute.data.subscribe(
       (response:any)=>{
         this.headerTitleService.updatedTitle(response.title);
         this.headerTitleService.updatedStart(response.start);
       }
     );
+    this.countryOptions = this.studentFormControls.country.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value,'countryList')),
+    );
+    this.csOptions = this.studentFormControls.studentCommunicationSkills.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value,'CommunicationSkills')),
+    );
+    this.tsOptions = this.studentFormControls.studentTechnicalSkills.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value,'TechnicalSkills')),
+    );
+    this.languageOptions = this.studentFormControls.studentLanguageKnown.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value,'LanguageKnown')),
+    );
   }
 
   get studentFormControls(){
     return this.studentForm.controls;
   }
+
+  private _filter(value: string, filterFor:string): string[] {
+    const filterValue = value.toLowerCase();
+    let options:any;
+    if(filterFor=='countryList'){
+      options=this.countryList.filter(option => option.toLowerCase().includes(filterValue));
+    }
+
+    if(filterFor=='CommunicationSkills'){
+      options=this.communicationSkill.filter(option => option.toLowerCase().includes(filterValue));
+    }
+
+    if(filterFor=='TechnicalSkills'){
+      options=this.techSkill.filter(option => option.toLowerCase().includes(filterValue));
+    }
+
+    if(filterFor=='LanguageKnown'){
+      options=this.languageSkill.filter(option => option.toLowerCase().includes(filterValue));
+    }
+
+    return options;
+  }
+
+  getCountryList(){
+    this.collageservices.getCountryList().subscribe(
+      response=>{
+        let countries:any;
+        countries=response
+        for(let i in countries){
+          this.countryList.push(countries[i].countryName)
+        }
+      }
+    );
+  }
+
 
   onSubmit(){
 this.submitted=true;
